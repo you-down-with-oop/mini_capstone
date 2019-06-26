3503325b691be6c1ce587b1f5796e0d1806eb081
Peter Jang <peter.x.jang@gmail.com>
2019-06-25 20:17:52 -0500

    Add one product display actions for first three products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def first_product_method
+    @product = Product.first
+    render "one_product.json.jb"
+  end
+
+  def second_product_method
+    @product = Product.second
+    render "one_product.json.jb"
+  end
+
+  def third_product_method
+    @product = Product.third
+    render "one_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products_url" => "products#all_products_method"
+    get "/first_product_url" => "products#first_product_method"
+    get "/second_product_url" => "products#second_product_method"
+    get "/third_product_url" => "products#third_product_method"
   end
 end
