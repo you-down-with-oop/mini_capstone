e1e79afb9d9c70186998b65679e2180f15641b23
Peter Jang <peter.x.jang@gmail.com>
2019-07-01 18:57:11 -0500

    Add validations to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 5..500 }
+
   def is_discounted?
     price <= 10
   end
