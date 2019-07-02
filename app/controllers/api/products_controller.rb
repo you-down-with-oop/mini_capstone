f714590319cd7610fbda3786c6b96d3c3b049325
Peter Jang <peter.x.jang@gmail.com>
2019-07-01 19:17:41 -0500

    Add happy/sad paths for products create and update actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -11,8 +11,11 @@ class Api::ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description],
     )
-    @product.save
-    render "show.json.jb"
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -26,8 +29,11 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
-    @product.save
-    render "show.json.jb"
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
