18d2b95f32186af02465f95cccd76e9387956e69
Peter Jang <peter.x.jang@gmail.com>
2019-07-03 18:24:13 -0500

    Add supplier model with database connection to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
-  validates :description, length: { in: 5..500 }
+  validates :description, length: { in: 5..1000 }
 
   def is_discounted?
     price <= 10
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_231726) do
+ActiveRecord::Schema.define(version: 2019_07_03_230902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_07_01_231726) do
     t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
