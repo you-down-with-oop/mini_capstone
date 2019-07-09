02ca7170f7b5a6108ef2a03ca352decabb73a6e6
Peter Jang <peter.x.jang@gmail.com>
2019-07-08 21:14:31 -0500

    Authorize orders controller to require being logged in

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,30 +1,42 @@
 class Api::OrdersController < ApplicationController
   def index
-    @orders = current_user.orders
-    render "index.json.jb"
+    if current_user
+      @orders = current_user.orders
+      render "index.json.jb"
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
+    if current_user
+      product = Product.find_by(id: params[:product_id])
 
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = product.tax * params[:quantity].to_i
-    calculated_total = product.total * params[:quantity].to_i
+      calculated_subtotal = product.price * params[:quantity].to_i
+      calculated_tax = product.tax * params[:quantity].to_i
+      calculated_total = product.total * params[:quantity].to_i
 
-    @order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
-    )
-    @order.save
-    render "show.json.jb"
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total,
+      )
+      @order.save
+      render "show.json.jb"
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def show
-    @order = current_user.orders.find_by(id: params[:id])
-    render "show.json.jb"
+    if current_user
+      @order = current_user.orders.find_by(id: params[:id])
+      render "show.json.jb"
+    else
+      render json: [], status: :unauthorized
+    end
   end
 end
