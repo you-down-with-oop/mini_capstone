9cc0178782d80ce6e0813c73d8fd8f5e2c7d805b
Peter Jang <peter.x.jang@gmail.com>
2019-07-08 21:18:49 -0500

    Refactor authorization using before_action

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,42 +1,32 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    if current_user
-      @orders = current_user.orders
-      render "index.json.jb"
-    else
-      render json: [], status: :unauthorized
-    end
+    @orders = current_user.orders
+    render "index.json.jb"
   end
 
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
+    product = Product.find_by(id: params[:product_id])
 
-      calculated_subtotal = product.price * params[:quantity].to_i
-      calculated_tax = product.tax * params[:quantity].to_i
-      calculated_total = product.total * params[:quantity].to_i
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = product.tax * params[:quantity].to_i
+    calculated_total = product.total * params[:quantity].to_i
 
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total: calculated_total,
-      )
-      @order.save
-      render "show.json.jb"
-    else
-      render json: [], status: :unauthorized
-    end
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+    @order.save
+    render "show.json.jb"
   end
 
   def show
-    if current_user
-      @order = current_user.orders.find_by(id: params[:id])
-      render "show.json.jb"
-    else
-      render json: [], status: :unauthorized
-    end
+    @order = current_user.orders.find_by(id: params[:id])
+    render "show.json.jb"
   end
 end
