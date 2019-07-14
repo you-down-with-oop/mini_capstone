2755db05bdc300fd95d099e113502baee40f5f23
Peter Jang <peter.x.jang@gmail.com>
2019-07-14 09:50:01 -0500

    Add orders create action (fix to use with shopping cart)

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,21 +7,30 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = product.tax * params[:quantity].to_i
-    calculated_total = product.total * params[:quantity].to_i
+    carted_products = current_user.carted_products.where(status: "carted")
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+      calculated_tax += carted_product.quantity * carted_product.product.tax
+      calculated_total += carted_product.quantity * carted_product.product.total
+    end
 
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: calculated_subtotal,
       tax: calculated_tax,
       total: calculated_total,
     )
     @order.save
+
+    carted_products.each do |carted_product|
+      carted_product.status = "purchased"
+      carted_product.order_id = @order.id
+      carted_product.save
+    end
+
     render "show.json.jb"
   end
 
