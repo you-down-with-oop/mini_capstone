6400f273358646fda760d57add693b763c987725
Peter Jang <peter.x.jang@gmail.com>
2019-07-14 10:37:25 -0500

    Refactor order create to move logic into model method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,30 +7,13 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-    calculated_subtotal = 0
-    calculated_tax = 0
-    calculated_total = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-      calculated_tax += carted_product.quantity * carted_product.product.tax
-      calculated_total += carted_product.quantity * carted_product.product.total
-    end
-
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
     )
     @order.save
-
-    carted_products.each do |carted_product|
-      carted_product.status = "purchased"
-      carted_product.order_id = @order.id
-      carted_product.save
-    end
-
+    carted_products = current_user.carted_products.where(status: "carted")
+    carted_products.update_all(status: "purchased", order_id: @order.id)
+    @order.update_totals
     render "show.json.jb"
   end
 
