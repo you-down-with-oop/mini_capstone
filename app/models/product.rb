a3f0f5270983cfede72f96bcb457b88955a06685
Peter Jang <peter.x.jang@gmail.com>
2019-07-14 09:16:59 -0500

    Add carted products model with associations
    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,7 +16,8 @@ class Product < ApplicationRecord
   #   Image.where(product_id: self.id)
   # end
 
-  has_many :orders
+  has_many :carted_products
+  has_many :orders, through: :carted_products
   has_many :category_products
   has_many :categories, :through => :category_products
   # def categories
